7ad42ef1d9a482755d167ab4c77cc23c1dd322d5
Stratila Dumitru <stratila.mail@gmail.com>
2022-07-30 18:30:49 +0300

    Homework lesso6

---
+++ FirstPoject/src/Lesson6.java
@@ -0,0 +1,11 @@
+public class Lesson6 {
+    public static void main(String[] args){
+        int a = 12;
+        if (a%2==0 && a>10){
+            System.out.println("Numarul este par");
+        }
+else if (a%2!=0 && a<15){
+            System.out.println("Numarul este impar");
+        }
+    }
+}
