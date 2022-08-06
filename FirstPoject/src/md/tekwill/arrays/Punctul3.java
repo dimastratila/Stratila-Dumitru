e4c0c8685009661d234ed36069d89cba49827edf
Stratila Dumitru <stratila.mail@gmail.com>
2022-08-06 18:37:40 +0300

    Exercitiu array modificat

--- FirstPoject/src/md/tekwill/arrays/Punctul3.java
+++ FirstPoject/src/md/tekwill/arrays/Punctul3.java
@@ -19,6 +19,7 @@ public class Punctul3 {
 for (int i=0;i<arr2.length;i++)
     System.out.println(arr2[i]);
 
+
         return arr2;
 }
 
@@ -28,8 +29,8 @@ for (int i=0;i<arr2.length;i++)
     public static void main(String[] args){
         int[] arr = {1,2,3,4,5,6,7,8,9,10};
 
+    Array(arr);
 
-       System.out.println(Array(arr));
 
 
     }
