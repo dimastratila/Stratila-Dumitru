e356d626482e1cd1d8edeb4d94d23612cbb3946b
Stratila Dumitru <stratila.mail@gmail.com>
2022-08-07 06:29:59 +0300

    Punctul 1 modificat

--- FirstPoject/src/md/tekwill/arrays/Punctul1.java
+++ FirstPoject/src/md/tekwill/arrays/Punctul1.java
@@ -5,7 +5,7 @@ public class Punctul1 {
 
 
 public static int Array(int[] num, int a){
-   int k=0;
+   int k=-1;
    for(int i=0;i<num.length;i++){
        if (num[i]==a){
            k=i;
@@ -15,9 +15,9 @@ public static int Array(int[] num, int a){
    }
 
 
-if (k!=0)
+
 return k;
-else return -1;
+
 
 
 }
@@ -27,7 +27,7 @@ public static void main(String[] args){
  int[] num = {1,2,3,4};
 
 
-    System.out.println(Array(num,2));
+    System.out.println(Array(num,4));
 
 
 }
